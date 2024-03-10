5551bc1123349fa638c61f473e08b9eb57cd0046
Iner <inercollado52@gmail.com>
2024-03-10 22:00:08 +0800

    can now sign up [add data to database] cool!

---
+++ LoginForm/signup.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace LoginForm
+{
+    partial class signup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_userame = new System.Windows.Forms.TextBox();
+            this.lbl_password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_cpassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_signup = new System.Windows.Forms.Button();
+            this.lbl_login = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(58, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // lbl_userame
+            // 
+            this.lbl_userame.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_userame.Location = new System.Drawing.Point(62, 112);
+            this.lbl_userame.Name = "lbl_userame";
+            this.lbl_userame.Size = new System.Drawing.Size(237, 30);
+            this.lbl_userame.TabIndex = 2;
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_password.Location = new System.Drawing.Point(62, 176);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(237, 30);
+            this.lbl_password.TabIndex = 5;
+            this.lbl_password.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(58, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // txt_cpassword
+            // 
+            this.txt_cpassword.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_cpassword.Location = new System.Drawing.Point(62, 239);
+            this.txt_cpassword.Name = "txt_cpassword";
+            this.txt_cpassword.Size = new System.Drawing.Size(237, 30);
+            this.txt_cpassword.TabIndex = 7;
+            this.txt_cpassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(58, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Confirm Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(117, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sign Up";
+            // 
+            // btn_signup
+            // 
+            this.btn_signup.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_signup.FlatAppearance.BorderSize = 0;
+            this.btn_signup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_signup.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_signup.Location = new System.Drawing.Point(62, 292);
+            this.btn_signup.Name = "btn_signup";
+            this.btn_signup.Size = new System.Drawing.Size(237, 34);
+            this.btn_signup.TabIndex = 9;
+            this.btn_signup.Text = "Sign Up";
+            this.btn_signup.UseVisualStyleBackColor = false;
+            this.btn_signup.Click += new System.EventHandler(this.btn_signup_Click);
+            // 
+            // lbl_login
+            // 
+            this.lbl_login.AutoSize = true;
+            this.lbl_login.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_login.ForeColor = System.Drawing.Color.Cyan;
+            this.lbl_login.Location = new System.Drawing.Point(58, 329);
+            this.lbl_login.Name = "lbl_login";
+            this.lbl_login.Size = new System.Drawing.Size(63, 19);
+            this.lbl_login.TabIndex = 11;
+            this.lbl_login.Text = "Log in";
+            this.lbl_login.Click += new System.EventHandler(this.lbl_login_Click);
+            // 
+            // signup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(344, 426);
+            this.Controls.Add(this.lbl_login);
+            this.Controls.Add(this.btn_signup);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_cpassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_userame);
+            this.Controls.Add(this.label1);
+            this.Name = "signup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "signup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lbl_userame;
+        private System.Windows.Forms.TextBox lbl_password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_cpassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_signup;
+        private System.Windows.Forms.Label lbl_login;
+    }
+}
---
+++ LoginForm/signup.cs
@@ -0,0 +1,110 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class signup : Form
+    {
+        public signup()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_login_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.Show();
+
+            this.Close();
+        }
+
+        public void signup_() {
+            string query = "INSERT INTO userlogin (Username, Password) VALUES (@username, @password)";
+            MySqlConnection conn = DBConnection.getConnection();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@username", lbl_userame.Text);
+            cmd.Parameters.AddWithValue("@password", lbl_password.Text);
+
+            if(lbl_password.Text == txt_cpassword.Text)
+            {
+                if(!IsUsernameAvailable(lbl_userame.Text))
+                {
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Data Inserted Successfully");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Data Insertin failed");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                } 
+                
+            } 
+            else
+            {
+                MessageBox.Show("Incorrect details");
+            }
+                
+        }
+
+        private bool IsUsernameAvailable(string username)
+        {
+            string query = "SELECT * FROM userlogin WHERE Username = @username";
+            MySqlConnection conn = DBConnection.getConnection();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue ("@username", username);
+            conn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            try
+            {
+                
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("Username already exists");
+                    return true;
+                } else
+                {
+                    
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            } 
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btn_signup_Click(object sender, EventArgs e)
+        {
+            signup_();
+            lbl_userame.Text = "";
+            lbl_password.Text = "";
+            txt_cpassword.Text = "";
+        }
+    }
+}
